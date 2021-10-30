ea42ad52014c04991428b4ef478d8040c2e94764
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-30 17:13:08 +0530

    Added: UC3_UserRegistration

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,8 +3,9 @@ package UserRegistrationDemo;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean lastName() {
-        boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,10}$", "Labade");
+    public boolean email() {
+        boolean matchesString = Pattern.matches("^[A-Za-z._]+[@]{1}[a-zA-Z]+[.a-z]{4}([.a-zA-Z]{3})*$",
+                "rahullabade05@gmail.com");
         return matchesString;
     }
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -4,10 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+    private Assertions Assert;
+
     @Test
-    public void givenLastNameShouldWrittenTrue(){
+    public void givenEmailWhenShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.lastName();
+        boolean userTest = userRegistration.email();
         Assert.assertTrue(userTest);
     }
 }
