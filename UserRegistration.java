3a60c8edaac86bb899d26b5470b007aea6fc29bc
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-30 17:45:41 +0530

    Added: UC6_UserRegistration

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,8 +3,8 @@ package UserRegistrationDemo;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean password() {
-        boolean matchesString = Pattern.matches("^[a-z]{5}$", "rahul");
+    public boolean passwordUpperCase() {
+        boolean matchesString = Pattern.matches("^[A-Z{1}a-z]{5}$", "rahul");
         return matchesString;
     }
 }
