d183dc342a2bf3af44380d08480cd5108ce825c2
RAHUL LABADE <rahullabade05@gmail.com>
2021-10-30 17:32:42 +0530

    Added: UC5_UserRegistration

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,8 +3,8 @@ package UserRegistrationDemo;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-        public boolean phoneNumber() {
-            boolean matchesString = Pattern.matches("^[0-9]{2}[ ][789]{1}[0-9]{9}$", "91 8390970948");
-            return matchesString;
-        }
+    public boolean password() {
+        boolean matchesString = Pattern.matches("^[a-z]{5}$", "rahul");
+        return matchesString;
+    }
 }
